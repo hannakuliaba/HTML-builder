85ff9e4064b863998b67096d1b35dd72a3a98e92
hannakuliaba <hanna.kuliaba@gmail.com>
2024-01-24 17:57:25 +0100

    fix: fix solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,10 +3,22 @@ const path = require('path');
 const newDirectory = path.join(__dirname, 'files-copy');
 const promise = fs.promises.readdir(path.join(__dirname, 'files'));
 
-fs.mkdir(newDirectory, { recursive: true }, (error) => {
-	if (error) {
-		console.log(error);
-	}
+fs.promises.readdir(path.join(__dirname, 'files-copy')).then(files => {
+	for (const file of files) {
+		fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+		});
+	};
+	copyFiles();
+}).catch(error => {
+	fs.mkdir(newDirectory, { recursive: true }, (error) => {
+		if (error) {
+			console.log(error);
+		}
+	});
+	copyFiles();
+});
+
+function copyFiles() {
 	promise.then(files => {
 		for (const file of files) {
 			fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', path.basename(file)), (error) => {
@@ -18,4 +30,4 @@ fs.mkdir(newDirectory, { recursive: true }, (error) => {
 	}).catch(err => {
 		console.log(err);
 	});
-});
+};
